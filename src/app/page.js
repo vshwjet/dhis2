0e7c2080df77a95122960dd96757b4ea952572d5
Vishwajeet <vishwajeet.deshmukh@juspay.in>
2023-11-30 18:39:41 +0530

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,225 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <main>
+      <HeroSection />
+      <Features />
+    </main>
+  );
+}
+
+const HeroSection = () => {
+  return (
+    <>
+      <div className="bg-[#12191E] min-h-screen">
+        <Header />
+        <div className="container flex items-center justify-between mx-auto my-32 px-4">
+          <div className="hero-text text-white font-[Satoshi]">
+            <h1 className="text-5xl font-medium leading-[1.3]">
+              Revolutionizing Data-Driven <br /> Healthcare Worldwide
+            </h1>
+            <p className="text-xl mt-[24px] leading-[140%]">
+              The world's largest health information management system —
+              <br />
+              developed through global collaboration led by University of Oslo
+            </p>
+            <div className="resources bg-[#20272D] p-8 mt-[60px] rounded-[10px]">
+              <h3 className="mb-6 text-[28px] leading-[120%]">
+                Explore the Latest Releases
+              </h3>
+              <div className="resource-links text-[#3994FF]">
+                <Link
+                  href={"/"}
+                  className="flex items-center text-[18px] mb-[22px] gap-2.5"
+                >
+                  DHIS2 Version 40 Overview
+                  <Image
+                    src="/assets/right-arrow.svg"
+                    width={24}
+                    height={24}
+                    alt="Dhis2 Logo"
+                  />
+                </Link>
+                <Link
+                  href={"/"}
+                  className="flex items-center text-[18px] gap-2.5"
+                >
+                  Android Capture App v2.8 Overview
+                  <Image
+                    src="/assets/right-arrow.svg"
+                    width={24}
+                    height={24}
+                    alt="Dhis2 Logo"
+                  />
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="hero-image">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src="/assets/hero-image.png"
+              width={773}
+              height={550}
+              alt="Dhis2 Logo"
             />
-          </a>
+          </div>
         </div>
       </div>
+    </>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+const Header = () => {
+  const navLinks = [
+    { id: 1, linkName: "About", linK: "/" },
+    { id: 2, linkName: "Features", linK: "/" },
+    { id: 3, linkName: "Resources", linK: "/" },
+    { id: 4, linkName: "Academy", linK: "/" },
+    { id: 5, linkName: "Health", linK: "/" },
+    { id: 6, linkName: "Logistics", linK: "/" },
+    { id: 7, linkName: "Education", linK: "/" },
+    { id: 8, linkName: "Downloads", linK: "/" },
+  ];
+
+  return (
+    <>
+      <div className="container flex justify-between items-center py-8 px-4 mx-auto">
+        <div className="logo">
+          <Image
+            src="/assets/dhis2-logo.svg"
+            width={125}
+            height={38}
+            alt="Dhis2 Logo"
+          />
+        </div>
+        <div className="nav">
+          <div className="flex gap-8">
+            {navLinks.map((item) => (
+              <Link key={item.id} href={item.linK}>
+                <article className="text-base font-[Satoshi] font-regular text-white">
+                  {item.linkName}
+                </article>
+              </Link>
+            ))}
+            <Link href="/">
+              <span className="flex gap-1.5">
+                <article className="text-[#EE6161] font-[Satoshi] text-base">
+                  COVID-19
+                </article>
+                <Image
+                  src="/assets/Coronavirus.svg"
+                  width={14}
+                  height={14}
+                  alt="Dhis2 Logo"
+                />
+              </span>
+            </Link>
+          </div>
+        </div>
       </div>
+    </>
+  );
+};
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+const Features = () => {
+  return (
+    <>
+      <div className="feature-cards container px-4 mx-auto my-[100px]">
+        <div className="cardsheader flex flex-col items-center">
+          <h3 className="text-4xl w-[711px] text-center font-[Satoshi] font-medium leading-[120%] mb-12">
+            <span className="text-[#1B85FF]">
+              Collect, manage, visualize & analyze
+            </span>{" "}
+            your data — all in one place
+          </h3>
+          <p className="text-[#021a3e] w-[844px] text-center font-['Satoshi'] text-xl font-medium leading-[140%]">
+            DHIS2 is a versatile, open-source software platform, customizable
+            for various uses like health, logistics, or education. It integrates
+            data from multiple sources into one unified system.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </div>
+        <div className="cards-container grid grid-cols-4 gap-[30px] grid-rows-2 mt-[78px]">
+          <div className="col-span-1 row-span-2 card-single-1 rounded-xl bg-[#eaecf0] p-[42px] overflow-clip">
+            <article className="text-[#021a3e] font-['Satoshi'] text-2xl font-medium leading-[120%]">
+              Analytics and Visualization
+            </article>
+            <p className="text-[#2b2b2b] mt-[14px] font-['Satoshi'] font-medium leading-[160%]">
+              Use maps, charts, and pivot tables to explore your data.
+            </p>
+            <Image
+              src="/assets/laptop-analytics.png"
+              width={572}
+              height={772}
+              alt="Laptop showing analytics"
+              className="mt-[45px] w-[572] h-auto max-w-none"
+            />
+          </div>
+          <div className="col-span-2 row-span-1 card-single-2 rounded-xl bg-[#eaecf0] p-[42px] overflow-clip flex flex-row items-end relative">
+            <div>
+              <article className="text-[#021a3e] font-['Satoshi'] text-2xl font-medium leading-[120%]">
+                Enter Data Anywhere
+              </article>
+              <p className="text-[#2b2b2b] mt-[14px] font-['Satoshi'] font-medium leading-[160%] w-[375px]">
+                Capture and view individual and aggregate data using custom
+                forms on a wide range of devices
+              </p>
+            </div>
+            <Image
+              src="/assets/phone.png"
+              width={304}
+              height={772}
+              alt="Laptop showing analytics"
+              className="w-[304px] h-auto max-w-none absolute top-[30px] right-0"
+            />
+          </div>
+          <div className="col-start-2 col-span-1 row-start-2 card-single-3 rounded-xl bg-[#eaecf0] p-[42px] overflow-clip flex flex-row items-end relative">
+            <div>
+              <article className="text-[#021a3e] font-['Satoshi'] text-2xl font-medium leading-[120%]">
+                Case Management
+              </article>
+              <p className="text-[#2b2b2b] mt-[14px] font-['Satoshi'] font-medium leading-[160%]">
+                With Tracker, you can collect and analyze case-based data to
+                track a person or entity over time
+              </p>
+            </div>
+          </div>
+          <div className="col-start-3 col-span-1 row-start-2 card-single-4 rounded-xl bg-[#eaecf0] p-[42px] overflow-clip flex flex-row items-end relative">
+            <div>
+              <article className="text-[#021a3e] font-['Satoshi'] text-2xl font-medium leading-[120%]">
+                Open Ecosystem
+              </article>
+              <p className="text-[#2b2b2b] mt-[14px] font-['Satoshi'] font-medium leading-[160%]">
+                Enhance your DHIS2 implementation with custom apps, metadata
+                packages, and other open-source tools
+              </p>
+            </div>
+          </div>
+          <div className="col-start-4 col-span-1 row-start-1 card-single-5 rounded-xl bg-[#eaecf0] p-[42px] overflow-clip flex flex-row items-end relative">
+            <div>
+              <article className="text-[#021a3e] font-['Satoshi'] text-2xl font-medium leading-[120%]">
+                Security and Privacy
+              </article>
+              <p className="text-[#2b2b2b] mt-[14px] font-['Satoshi'] font-medium leading-[160%]">
+                DHIS2 comes with a set of configurable privacy tools like user
+                management and sharing settings
+              </p>
+            </div>
+          </div>
+          <div className="col-start-4 col-span-1 row-start-2 card-single-6 rounded-xl bg-[#eaecf0] p-[42px] overflow-clip flex flex-row items-end relative">
+            <div>
+              <article className="text-[#021a3e] font-['Satoshi'] text-2xl font-medium leading-[120%]">
+                Internationalised
+              </article>
+              <p className="text-[#2b2b2b] mt-[14px] font-['Satoshi'] font-medium leading-[160%]">
+                You can translate your content and interface into as many
+                languages as you like, and users can switch languages
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-    </main>
-  )
-}
+    </>
+  );
+};
